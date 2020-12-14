8f93fb9669861f537873e8462186149da90e5a70
Haakon <aareskjold.haakon@gmail.com>
2020-12-14 19:35:11 +0100

    updated index html and test

--- index.test.js
+++ index.test.js
@@ -1,7 +1,7 @@
 function pipeline() {
-  return 'This is the end of the pipeline!';
+  return 'Here are some projects made by me:';
 }
 
-test('string outputs: This is the end of the pipeline!', () => {
-  expect(pipeline()).toMatch('This is the end of the pipeline!');
+test('string outputs: Here are some projects made by me:', () => {
+  expect(pipeline()).toMatch('Here are some projects made by me:');
 });
