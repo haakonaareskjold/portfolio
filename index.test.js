92f1f660e1c3dd076c9435de63ab0c1315365cb9
haakonaareskjold <aareskjold.hhaakon@gmail.com>
2021-02-22 17:37:08 +0100

    added sass and webpack

--- index.test.js
+++
@@ -1,7 +0,0 @@
-function pipeline() {
-  return 'Here are some projects made by me:';
-}
-
-test('string outputs: Here are some projects made by me:', () => {
-  expect(pipeline()).toMatch('Here are some projects made by me:');
-});
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+
+    mode: "production",
+
+    entry: './src/index.js',
+    resolve: {
+        extensions: ['.js']
+    },
+
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'public')
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.scss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: 'body',
+            template: './src/index.html',
+            filename: 'index.html'
+        })
+    ]
+};
