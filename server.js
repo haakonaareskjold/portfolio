81f20bee441657c132e4020003b0911275951cb9
haakonaareskjold <aareskjold.hhaakon@gmail.com>
2021-02-28 22:27:49 +0100

    changed port to hardcoded 80

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
-const PORT = process.env.PORT || 80;
 var express = require('express');
 
 var app = express();
 
-app.listen(PORT, ()  => {
-	console.log(` app listening at http://localhost:${PORT}`)
-});
-
+app.listen(80)
 
 app.use('/', express.static('public'))
 
