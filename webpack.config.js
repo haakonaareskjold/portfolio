8b8713092643dccb360f40bda981ba9b03b9c727
Haakon <aareskjold.haakon@gmail.com>
2021-03-02 14:03:34 +0100

    added optimization to wepack, changed responsiveness and added aria labels for anchors, compressed static asset size

--- webpack.config.js
+++ webpack.config.js
@@ -12,9 +12,26 @@ module.exports = {
     },
 
     output: {
-        filename: 'main.js',
+        filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'public')
     },
+    optimization: {
+        runtimeChunk: 'single',
+        splitChunks: {
+          chunks: 'all',
+          maxInitialRequests: Infinity,
+          minSize: 0,
+          cacheGroups: {
+            vendor: {
+              test: /[\\/]node_modules[\\/]/,
+              name(module) {
+                const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+                return `npm.${packageName.replace('@', '')}`;
+              },
+            },
+          },
+        },
+      },
 
     module: {
         rules: [
