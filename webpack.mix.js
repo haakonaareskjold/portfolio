72961ce04e8254ace445ab2dfc61610a392e23b1
haakonaareskjold <aareskjold.haakon@gmail.com>
2021-05-23 16:53:31 +0200

    adding browsersync, sourcemaps for dev, code chunking on prod

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,4 +4,10 @@ mix.js('src/js/main.js', 'js')
     .sass('src/sass/main.scss', 'css')
     .copy('src/static', 'public')
     .copy('src/assets', 'public/assets')
-    .setPublicPath('public');
+    .setPublicPath('public')
+    .sourceMaps(false, 'source-map')
+    .browserSync('127.0.0.1:3000');
+
+if (mix.inProduction()) {
+    mix.version();
+}
